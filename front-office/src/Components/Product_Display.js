90d8284103e3d0d4088ff7c58c0a5fe87b85e91c
Gerald <gerald.manzano@studio.unibo.it>
2023-02-20 16:29:16 +0100

    display products

--- front-office/src/Components/Product_Display.js
+++ front-office/src/Components/Product_Display.js
@@ -1,25 +1,59 @@
 import React from "react";
-import Product from "./Product";
+import './Product_Display.css'
 
 class Product_Display extends React.Component 
 {
     constructor(props) 
     {
         super(props);
-        this.state = props.productList;
+        this.state = {
+            screenWidth: window.innerWidth,
+            content: this.props.product.name
+        }
     }
+
+    handleResize = () =>{
+        this.setState({screenWidth: window.innerWidth});
+    }
+
+    handleHover = () =>{
+        this.setState({content:this.props.product.description})
+    }
+
+    handleUnhover = () =>{
+        this.setState({content:this.props.product.name})
+    }
+
+    componentDidMount() {
+        document.getElementById("product"+this.props.product._id).addEventListener("mouseover", this.handleHover);
+        document.getElementById("product"+this.props.product._id).addEventListener("mouseout", this.handleUnhover);
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    componentWillUnmount() {
+        document.getElementById("product"+this.props.product._id).removeEventListener("mouseover", this.handleHover);
+        document.getElementById("product"+this.props.product._id).removeEventListener("mouseout", this.handleUnhover);
+        window.removeEventListener("resize", this.handleResize);
+    }
+
+    product = this.props.product;
 
     render() 
     {
+        let id = "product"+this.props.product._id;
         return(
-            <div className="container">
-                <div className="row">
-                    {this.state.productList.map((product) => 
-                    {
-                        <div className="col-sm-4">
-                            <Product key={product.id} product={product} />
-                        </div>
-                    })}
+            <div id={id} className="container product">
+                <div>
+                    <div className="imgpContainer">
+                        <img className="imgp" src={this.product.img}></img>
+                    </div>
+                    <div className={`${this.state.content ==  this.props.product.name? "namep" : "descp"}`}>
+                        {this.state.content}
+                    </div>
+                </div>
+                <div>
+                    <div className="pricep"> {this.product.price}</div>
+                    <button className='btn-add'>ADD TO CART</button>
                 </div>
             </div>
         )
