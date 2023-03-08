cfa419b88e4ad6318d9645b6f4d5fbf95f0b926e
Leon <leon.pirazzoli@studio.unibo.it>
2023-03-08 21:59:24 +0100

    VAAAAAAAAA (le prenotazioni)

--- front-office/src/Pages/Profile.js
+++ front-office/src/Pages/Profile.js
@@ -21,7 +21,6 @@ export default function Profile() {
     useEffect(() => 
     {
         //Get bookings
-        debugger;
         if(!bookings && profileInfo) {
             console.log("old bookings: " + bookings + ", fetching bookings");
             fetch(`http://localhost:8000/db/getUserBookings?id=${profileInfo._id}`)
@@ -65,11 +64,12 @@ export default function Profile() {
     const deleteBooking = (e) => (booking) =>
     {
         // updating state bookings
+        /*debugger;
         let deletedIndex = bookings.findIndex((arrBooking) => arrBooking._id == booking._id);
         let remainingBookings = [];
         remainingBookings = bookings;
-        remainingBookings.splice(deletedIndex, 1);
-        setBookings(null);
+        remainingBookings.splice(deletedIndex, 1);*/
+        
         let obj = {
             collection: "bookings",
             id: booking._id
@@ -84,6 +84,13 @@ export default function Profile() {
             body: JSON.stringify(obj)
         }).then((res) => 
         {
+            fetch(`http://localhost:8000/db/getUserBookings?id=${profileInfo._id}`)
+            .then((res) => res.json())
+            .then((data) => 
+            {
+                setBookings(data);
+                //rotella stop
+            });
             //service availability update
             let service;
             fetch(`http://localhost:8000/db/element?collection=services&id=${booking.serviceId}`, {method: "GET"}).then((res) => res.json())
@@ -100,6 +107,7 @@ export default function Profile() {
                 });
             })
         })
+        //rotella start
     }
 
     const displayBookings = () => 
