9431fa89f8c3eaf36b71f5aca6dc9477656f9a7f
Leon <leon.pirazzoli@studio.unibo.it>
2023-03-09 16:29:51 +0100

    buildato

--- front-office/src/Components/Navbar.js
+++ front-office/src/Components/Navbar.js
@@ -17,7 +17,7 @@ class Navbar extends React.Component {
   loginBtn = () =>
   {
     if(JSON.parse(localStorage.getItem("login")).islogged){
-      if(window.location.pathname == '/profile/'){
+      if(window.location.pathname == '/profile/' || window.location.pathname == '/profile' ){
         return(<a className="icon-btn nav-link" onClick={this.logoutBtn} href="/profile">LOGOUT</a>);
       }
       return(<a className="icon-btn nav-link" href="/profile">PROFILE</a>);
@@ -103,7 +103,7 @@ class Navbar extends React.Component {
                   Cat products
                 </a>
               </li>
-              {displayBackOfficeAccess()}
+              {this.displayBackOfficeAccess()}
             </ul>
           </div>
         </div>
