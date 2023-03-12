cc91f848c248ee50168e9b43bace7fab7ae49cd3
Giuseppe Narcisi <giuseppe.narcisi@studio.unibo.it>
2023-03-12 18:18:29 +0100

    stringify

--- public/BackOffice/frontend/static/js/views/utenti/utenti.js
+++ public/BackOffice/frontend/static/js/views/utenti/utenti.js
@@ -9,6 +9,7 @@ fetch('/db/collection?collection=users', {
         for (var i = 0; i < clients.length; i++) {
             let client = clients[i];
             adminHtml[i] = '';
+            client._id = JSON.stringify(client._id);
             console.log(client._id);
             debugger;
             clientsHtml += `
