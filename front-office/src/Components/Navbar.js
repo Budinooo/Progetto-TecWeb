816eaa317f3e73ab5a4ead675b5205aac665f8ce
Leon <leon.pirazzoli@studio.unibo.it>
2023-03-13 07:19:05 +0100

    inizio lavori su stile logo

--- front-office/src/Components/Navbar.js
+++ front-office/src/Components/Navbar.js
@@ -63,7 +63,7 @@ class Navbar extends React.Component {
     return (
       <nav className="navbar navbar-expand-lg">
         <a className="navbar-brand ms-4" href="/">
-          <img src="img/immagine.png" alt="Animal House Logo"/>
+          <div className="logo"><span>A</span>NIMAL<span>H</span><img src="dog-house.png" alt="" />USE</div>
         </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
